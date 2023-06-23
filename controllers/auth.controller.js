8f93e601422c7e00017931dd62f362ca1c472b20
Raj Prakash Maurya <raj232033@gmail.com>
2023-06-23 11:21:10 +0100

    auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,4 @@
+const constants = require("../utils/constants");
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const dbConfig = require("./configs/db.config");
 const serverConfig = require("./configs/server.config");
 const bodyParser = require("body-parser");
+const User = require("./models/user.model");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connectin to mongodb
 mongoose.connect(dbConfig.DB_URL);
 const db = mongoose.connection;
 db.on("error", () => {
@@ -15,7 +18,32 @@ db.on("error", () => {
 });
 db.once("open", () => {
   console.log("connected to mongodb");
+
+  //create an admin doesnot exist
+  init();
 });
+
+async function init() {
+  var user = await User.findOne({ userId: "admin" });
+
+  if (user) {
+    console.log("admin is already present");
+    return;
+  }
+  try {
+    user = await User.create({
+      name: "Raj Prakash Maurya",
+      userId: "admin",
+      email: "rajprakashmaurya@gmail.com",
+      userType: "admin",
+      userStatus: "APPROVED",
+      password: bcrypt.hashSync("raj@9695", 8),
+    });
+    console.log(user);
+  } catch (e) {
+    console.log("Error while creating  admin user" + e);
+  }
+}
 
 //server is listining at the port
 app.listen(serverConfig.PORT, () => {
