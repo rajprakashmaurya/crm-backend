e4b34d7cd065dca6107df94b92ea0fd16fddea23
Raj Prakash Maurya <raj232033@gmail.com>
2023-06-27 06:26:46 +0100

    auth config

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,3 +2,8 @@ const constants = require("../utils/constants");
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const authConfig = require("../configs/auth.config");
+
+// exports.signup = async(req, res, ) =>{
+//   var
+// }
