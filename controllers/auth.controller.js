6f2d9cc21c4e17351450643fe783e38f14854919
Raj Prakash Maurya <raj232033@gmail.com>
2023-06-27 07:52:43 +0100

    auth controller signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,39 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const authConfig = require("../configs/auth.config");
 
-// exports.signup = async(req, res, ) =>{
-//   var
-// }
+exports.signup = async (req, res) => {
+  var userStatus = req.body.userStatus;
+  var userType = req.body.userType;
+  if (userType == constants.userTypes.customer) {
+    userStatus = constants.userStatus.approved;
+  } else {
+    userStatus = constants.userStatus.pending;
+  }
+
+  try {
+    const createUser = await User.create({
+      name: req.body.name,
+      userId: req.body.userId,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+      userType: req.body.userType,
+      userStatus: userStatus,
+    });
+
+    const postReponse = {
+      name: createUser.name,
+      userId: createUser.userId,
+      email: createUser.email,
+      userType: createUser.userType,
+      userStatus: createUser.userStatus,
+      createdAt: createUser.createdAt,
+      updatedAt: createUser.updatedAt,
+    };
+    response.status(200).send(postReponse);
+  } catch (e) {
+    console.log("Error occured while creating the user");
+    response.status(500).send({
+      message: "some internal error occured while creating the user",
+    });
+  }
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,10 @@
-module.exports = function () {};
+const authController = require("../controllers/auth.controller");
+const verifySignUp = require("../middlewares/verifySignUp");
+
+module.exports = function (app) {
+  app.post(
+    "/crm/api/v1/auth/signup",
+    [verifySignUp.validateSignUpRequest],
+    authController.signup
+  );
+};
