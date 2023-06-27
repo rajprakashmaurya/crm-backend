42682b51fb42b105604d1a606023ba6c8687177c
Raj Prakash Maurya <raj232033@gmail.com>
2023-06-27 08:35:17 +0100

    signIn complete

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const authConfig = require("../configs/auth.config");
 
+//for signUp
 exports.signup = async (req, res) => {
   var userStatus = req.body.userStatus;
   var userType = req.body.userType;
@@ -39,4 +40,42 @@ exports.signup = async (req, res) => {
       message: "some internal error occured while creating the user",
     });
   }
+};
+
+//for signIn method
+exports.signin = async (req, res) => {
+  const user = await User.findOne({
+    userId: req.body.userId,
+  });
+  if (!user) {
+    res.status(400).send({
+      message: "Failed! userId already exists",
+    });
+    return;
+  }
+  if (user.userStatus != constants.userStatus.approved) {
+    res.status(403).send({
+      message:
+        "Cannot  allow user to login as the status is " + user.userStatus,
+    });
+    return;
+  }
+  //check the password matches
+  var isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+  if (!isPasswordValid) {
+    res.status(401).send({
+      message: "Invalid password",
+    });
+  }
+  var token = jwt.sign({ id: user.userId }, authConfig.secretKey, {
+    expireIn: 86400,
+  });
+  res.status(200).send({
+    name: user.name,
+    userId: user.userId,
+    email: user.email,
+    userType: user.userType,
+    userStatus: user.userStatus,
+    accessToken: token,
+  });
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,4 +7,5 @@ module.exports = function (app) {
     [verifySignUp.validateSignUpRequest],
     authController.signup
   );
+  app.post("/crm/api/v1/auth/signin", authController.signin);
 };
